3052896139e0ea466bdbc964f375dcba91278779
veikkoruohonen <veikkoruohonen47@gmail.com>
2026-03-02 20:45:12 +0200

    Delete Karkausvuosi.py

--- Karkausvuosi.py
+++
@@ -1,6 +0,0 @@
-vuosi = int(input("Anna vuosiluku: "))
-
-if (vuosi % 400 == 0) or (vuosi % 4 == 0 and vuosi % 100 != 0):
-    print("Vuosi on karkausvuosi.")
-else:
-    print("Vuosi ei ole karkausvuosi.")
